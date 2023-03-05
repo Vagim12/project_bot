4bc564d2a607abac771bee584742b49a8c038e04
Vagim12 <123233855+Vagim12@users.noreply.github.com>
2023-03-05 17:38:44 +0300

    Update 1.py
    Пример работы с сообщениями пользователя.

--- 1.py
+++ 1.py
@@ -7,24 +7,13 @@ TOKEN = ""
 
 
 class YLBotClient(discord.Client):
-    async def on_ready(self):
-        logger.info(f'{self.user} has connected to Discord!')
-        for guild in self.quilds:
-            logger.info(
-                f'{self.user} подключились к чату:\n'
-                f'{guild.name}(id: {guild.id})')
-
-    async def on_member_join(self, member):
-        await member.create_dm()
-        await member.dm_channel.send(f"Привет, {member.name}!")
-
     async def on_message(self, message):
         if message.author == self.user:
             return
-        if "привет" in message.content.lower():
+        if "привет" in message.content.lower() and "бот" in message.content.lower():
             await message.channel.send("И тебе привет")
-        else:
-            await message.channel.send("Спасибо за сообщение")
+        if message.content.lower() == 'бот':
+            await message.channel.send("На месте!")
 
 
 intents = discord.Intents.default()
