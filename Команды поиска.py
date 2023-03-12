5009cac67625acc8d2e6f0cc769e1a2dfa6fed4f
Vagim12 <123233855+Vagim12@users.noreply.github.com>
2023-03-12 17:16:42 +0300

    Add files via upload
    Создана команда $yt, позволяющая по контексту сообщения пользователя боту находить видео на youtube.
    Также сделана команда $gif, которая предоставляет пользователю "гифку" по его запросу после команды, а также оставит ссылку с самого сайта, на который он может перейти, чтобы посмотреть остальные варианты gif-изображений.
    Ну и в конце простая команда $ball, представляющая собой волшебный шар, отвечающий на какой-либо вопрос.

---
+++ Команды поиска.py
@@ -0,0 +1,49 @@
+import random
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import re
+import urllib
+
+config = {
+    'token': 'OTEwMTc4OTEzMTY5MzIyMDQ1.GBPAtY._1mrq4HNtyZ4PT7fyKyjUTips12YRwVlw8fxwo',
+    'prefix': '$',}
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix=config['prefix'], intents=intents)
+
+@bot.command()
+async def ball(ctx):
+    response =[
+        'Без сомнения.', 
+        'Хорошая перспектива.', 
+        'Лучше об этом не думать.', 
+        'Не могу предсказать сейчас.',
+        'Мой ответ - нет.', 
+        'Мне кажется это плохая идея.',
+    ]
+
+    await ctx.send(random.choice(response))
+
+@bot.command()
+async def gif(msg, *, search):
+    query_string = urllib.parse.urlencode({"search_query": search})
+    html_content = urllib.request.urlopen("https://giphy.com/search/" + query_string)
+    search_results = re.findall(query_string[13:], html_content.read().decode())
+    await msg.send("https://giphy.com/search/" + search_results[0])    
+
+@bot.command()
+async def yt(msg, *, search):
+    query_string = urllib.parse.urlencode({"search_query": search})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
+    await msg.send("http://www.youtube.com/watch?v=" + search_results[0])
+
+bot.run(config['token'])
+
+
+
+
+
+
